2160d601d29f95e4525db67457d573e82e89b156
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-09-20 17:57:13 +0100

    Run all the things

--- lnbits/wallets/lnbitsconnect.py
+++ lnbits/wallets/lnbitsconnect.py
@@ -1,8 +1,7 @@
 from typing import AsyncGenerator, Dict, Optional
 
-from loguru import logger
-
 import websockets
+from loguru import logger
 
 from lnbits.settings import settings
 
@@ -52,19 +51,25 @@ class LNbitsConnectWallet(Wallet):
         # 1. create nostr event with type = create_invoice, funding source npub and amount
         # 2. broadcast event
         # 3. wait for response from funding source
-        # 4. decode response and 
-        
-        await self.stub_receive_specific_message("ws://localhost:8080", "INVOICE_CREATED")
-        
+        # 4. decode response and
+
+        await self.stub_receive_specific_message(
+            "ws://localhost:8080", "INVOICE_CREATED"
+        )
+
         data: Dict = {"amount": amount, "description_hash": "", "memo": memo or ""}
         if description_hash:
             data["description_hash"] = description_hash.hex()
         elif unhashed_description:
             data["description_hash"] = hashlib.sha256(unhashed_description).hexdigest()
-            
-        data["payment_hash"] = "c1c9721247a875ba60aa534b992b42c5a3da81e66537af435ee8835bd0eb97dc"
-        data["payment_request"] = "lnbc100n1pjskg4fsp5vpgpp83awjhs5asa3fl0hhezm30ftzwzywvq6m7za7jwfg32l27qpp5c8yhyyj84p6m5c922d9ej26zck3a4q0xv5m67s67azp4h58tjlwqdq2f38xy6t5wvxqzjccqpjrzjq2e0f6yh2eyluj4vmmz2gh205z8u5hn0gv69kcpvegpcum2dznl95zlteuqq92gqqyqqqqqqqqqqq7qqjq9qxpqysgqd46ug2c7dcdf6pglyuj4s2dfuxv8y5hxgadscftkw49vqwlxrv8s36ay746c52gqnrd276ch9pf8acuw5duj34v0n0z6wwyer052hhsqv36rn8"
-                    
+
+        data[
+            "payment_hash"
+        ] = "c1c9721247a875ba60aa534b992b42c5a3da81e66537af435ee8835bd0eb97dc"
+        data[
+            "payment_request"
+        ] = "lnbc100n1pjskg4fsp5vpgpp83awjhs5asa3fl0hhezm30ftzwzywvq6m7za7jwfg32l27qpp5c8yhyyj84p6m5c922d9ej26zck3a4q0xv5m67s67azp4h58tjlwqdq2f38xy6t5wvxqzjccqpjrzjq2e0f6yh2eyluj4vmmz2gh205z8u5hn0gv69kcpvegpcum2dznl95zlteuqq92gqqyqqqqqqqqqqq7qqjq9qxpqysgqd46ug2c7dcdf6pglyuj4s2dfuxv8y5hxgadscftkw49vqwlxrv8s36ay746c52gqnrd276ch9pf8acuw5duj34v0n0z6wwyer052hhsqv36rn8"
+
         return InvoiceResponse(
             True, data["payment_hash"], data["payment_request"], None
         )
