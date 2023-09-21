198fc934455823fbcbd24d4554a2c5aa5327fba5
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-09-21 16:33:40 +0100

    The Lnbits wallet connect funding source is now watching for DMs to or from the LNbits wallet service  on start up

--- lnbits/wallets/lnbitsconnect.py
+++ lnbits/wallets/lnbitsconnect.py
@@ -1,9 +1,21 @@
-from typing import AsyncGenerator, Dict, Optional
+import asyncio
+import hashlib
+import json
 
-import websockets
+import time
+from asyncio import Queue
+from threading import Thread
+
+from websocket import WebSocketApp
+from pydantic import BaseModel
+from secp256k1 import PublicKey
+
+from typing import AsyncGenerator, Dict, Optional, List, Callable
+
 from loguru import logger
 
 from lnbits.settings import settings
+from lnbits.helpers import urlsafe_short_hash
 
 from .base import (
     InvoiceResponse,
@@ -13,22 +25,290 @@ from .base import (
     Wallet,
 )
 
+class NostrEvent(BaseModel):
+    id: str = ""
+    pubkey: str
+    created_at: int
+    kind: int
+    tags: List[List[str]] = []
+    content: str = ""
+    sig: Optional[str]
+
+    def serialize(self) -> List:
+        return [0, self.pubkey, self.created_at, self.kind, self.tags, self.content]
+
+    def serialize_json(self) -> str:
+        e = self.serialize()
+        return json.dumps(e, separators=(",", ":"), ensure_ascii=False)
+
+    @property
+    def event_id(self) -> str:
+        data = self.serialize_json()
+        id = hashlib.sha256(data.encode()).hexdigest()
+        return id
+
+    def check_signature(self):
+        event_id = self.event_id
+        if self.id != event_id:
+            raise ValueError(
+                f"Invalid event id. Expected: '{event_id}' got '{self.id}'"
+            )
+        try:
+            pub_key = PublicKey(bytes.fromhex("02" + self.pubkey), True)
+        except Exception:
+            raise ValueError(
+                f"Invalid public key: '{self.pubkey}' for event '{self.id}'"
+            )
+
+        valid_signature = pub_key.schnorr_verify(
+            bytes.fromhex(event_id), bytes.fromhex(self.sig), None, raw=True
+        )
+        if not valid_signature:
+            raise ValueError(f"Invalid signature: '{self.sig}' for event '{self.id}'")
+
+    def stringify(self) -> str:
+        return json.dumps(dict(self))
+
+    def tag_values(self, tag_name: str) -> List[str]:
+        return [t[1] for t in self.tags if t[0] == tag_name]
+
+    def has_tag_value(self, tag_name: str, tag_value: str) -> bool:
+        return tag_value in self.tag_values(tag_name)
+
+class NostrClient:
+    def __init__(self):
+        self.recieve_event_queue: Queue = Queue()
+        self.send_req_queue: Queue = Queue()
+        self.ws: WebSocketApp = None
+        self.subscription_id = "nostrmarket-" + urlsafe_short_hash()[:32]
+
+    async def connect_to_nostrclient_ws(
+        self, on_open: Callable, on_message: Callable
+    ) -> WebSocketApp:
+        def on_error(_, error):
+            logger.warning(error)
+
+        logger.info(f"Subscribing to websockets for nostrclient extension")
+        ws = WebSocketApp(
+            f"ws://localhost:{settings.port}/nostrclient/api/v1/relay",
+            on_message=on_message,
+            on_open=on_open,
+            on_error=on_error,
+        )
+
+        wst = Thread(target=ws.run_forever)
+        wst.daemon = True
+        wst.start()
+
+        return ws
+
+    async def get_event(self):
+        value = await self.recieve_event_queue.get()
+        if isinstance(value, ValueError):
+            raise value
+        return value
+
+    async def run_forever(self):
+        def on_open(_):
+            logger.info("Connected to 'nostrclient' websocket")
+
+        def on_message(_, message):
+            self.recieve_event_queue.put_nowait(message)
+
+        running = True
+
+        while running:
+            try:
+                req = None
+                if not self.ws:
+                    self.ws = await self.connect_to_nostrclient_ws(on_open, on_message)
+                    # be sure the connection is open
+                    await asyncio.sleep(3)
+                req = await self.send_req_queue.get()
+
+                if isinstance(req, ValueError):
+                    running = False
+                    logger.warning(str(req))
+                else:
+                    logger.info(f"Sending request: {req}")
+                    self.ws.send(json.dumps(req))
+            except Exception as ex:
+                logger.warning(ex)
+                if req:
+                    await self.send_req_queue.put(req)
+                self.ws = None  # todo close
+                await asyncio.sleep(5)
+
+    async def publish_nostr_event(self, e: NostrEvent):
+        await self.send_req_queue.put(["EVENT", e.dict()])
+        
+    async def subscribe_wallet_service(
+        self,
+        public_keys: List[str],
+    ):
+        dm_time = int(time.time())
+        dm_filters = self._filters_for_direct_messages(public_keys, dm_time)
+
+        self.subscription_id = "lnbitsconnect-" + urlsafe_short_hash()[:32]
+        await self.send_req_queue.put(["REQ", self.subscription_id] + dm_filters)
+        # log dm_filters
+        logger.debug(dm_filters)
+
+        logger.info(
+            f"Subscribed to events for: {len(public_keys)} keys. New subscription id: {self.subscription_id}"
+        )
+
+    async def subscribe_merchants(
+        self,
+        public_keys: List[str],
+        dm_time=0,
+        stall_time=0,
+        product_time=0,
+        profile_time=0,
+    ):
+        dm_filters = self._filters_for_direct_messages(public_keys, dm_time)
+        stall_filters = self._filters_for_stall_events(public_keys, stall_time)
+        product_filters = self._filters_for_product_events(public_keys, product_time)
+        profile_filters = self._filters_for_user_profile(public_keys, profile_time)
+
+        merchant_filters = (
+            dm_filters + stall_filters + product_filters + profile_filters
+        )
+
+        self.subscription_id = "nostrmarket-" + urlsafe_short_hash()[:32]
+        await self.send_req_queue.put(["REQ", self.subscription_id] + merchant_filters)
+
+        logger.debug(
+            f"Subscribed to events for: {len(public_keys)} keys. New subscription id: {self.subscription_id}"
+        )
+
+    async def merchant_temp_subscription(self, pk, duration=10):
+        dm_filters = self._filters_for_direct_messages([pk], 0)
+        stall_filters = self._filters_for_stall_events([pk], 0)
+        product_filters = self._filters_for_product_events([pk], 0)
+        profile_filters = self._filters_for_user_profile([pk], 0)
+
+        merchant_filters = (
+            dm_filters + stall_filters + product_filters + profile_filters
+        )
+
+        subscription_id = "merchant-" + urlsafe_short_hash()[:32]
+        logger.debug(
+            f"New merchant temp subscription ({duration} sec). Subscription id: {subscription_id}"
+        )
+        await self.send_req_queue.put(["REQ", subscription_id] + merchant_filters)
+
+        async def unsubscribe_with_delay(sub_id, d):
+            await asyncio.sleep(d)
+            await self.unsubscribe(sub_id)
+
+        asyncio.create_task(unsubscribe_with_delay(subscription_id, duration))
+
+    async def user_profile_temp_subscribe(self, public_key: str, duration=5) -> List:
+        try:
+            profile_filter = [{"kinds": [0], "authors": [public_key]}]
+            subscription_id = "profile-" + urlsafe_short_hash()[:32]
+            logger.debug(
+                f"New user temp subscription ({duration} sec). Subscription id: {subscription_id}"
+            )
+            await self.send_req_queue.put(["REQ", subscription_id] + profile_filter)
+
+            async def unsubscribe_with_delay(sub_id, d):
+                await asyncio.sleep(d)
+                await self.unsubscribe(sub_id)
+
+            asyncio.create_task(unsubscribe_with_delay(subscription_id, duration))
+        except Exception as ex:
+            logger.debug(ex)
+
+    def _filters_for_direct_messages(self, public_keys: List[str], since: int) -> List:
+        in_messages_filter = {"kinds": [4], "#p": public_keys}
+        out_messages_filter = {"kinds": [4], "authors": public_keys}
+        if since and since != 0:
+            in_messages_filter["since"] = since
+            out_messages_filter["since"] = since
+
+        return [in_messages_filter, out_messages_filter]
+
+    def _filters_for_stall_events(self, public_keys: List[str], since: int) -> List:
+        stall_filter = {"kinds": [30017], "authors": public_keys}
+        if since and since != 0:
+            stall_filter["since"] = since
+
+        return [stall_filter]
+
+    def _filters_for_product_events(self, public_keys: List[str], since: int) -> List:
+        product_filter = {"kinds": [30018], "authors": public_keys}
+        if since and since != 0:
+            product_filter["since"] = since
+
+        return [product_filter]
+
+    def _filters_for_user_profile(self, public_keys: List[str], since: int) -> List:
+        profile_filter = {"kinds": [0], "authors": public_keys}
+        if since and since != 0:
+            profile_filter["since"] = since
+
+        return [profile_filter]
+
+    async def restart(self):
+        await self.unsubscribe_merchants()
+        # Give some time for the CLOSE events to propagate before restarting
+        await asyncio.sleep(10)
+
+        logger.info("Restating NostrClient...")
+        await self.send_req_queue.put(ValueError("Restarting NostrClient..."))
+        await self.recieve_event_queue.put(ValueError("Restarting NostrClient..."))
+
+        self.ws.close()
+        self.ws = None
+
+    async def stop(self):
+        await self.unsubscribe_merchants()
+
+        # Give some time for the CLOSE events to propagate before closing the connection
+        await asyncio.sleep(10)
+        self.ws.close()
+        self.ws = None
+
+    async def unsubscribe_merchants(self):
+        await self.send_req_queue.put(["CLOSE", self.subscription_id])
+        logger.debug(
+            f"Unsubscribed from all merchants events. Subscription id: {self.subscription_id}"
+        )
+
+    async def unsubscribe(self, subscription_id):
+        await self.send_req_queue.put(["CLOSE", subscription_id])
+        logger.debug(f"Unsubscribed from subscription id: {subscription_id}")
+
 
 class LNbitsConnectWallet(Wallet):
     def __init__(self):
-        self.nsec = settings.lnbits_connect_nsec
-        self.funding_source_npub = settings.lnbits_connect_npub
-        # if not endpoint or not key:
-        #     raise Exception("cannot initialize lntxbod")
-        # self.endpoint = endpoint[:-1] if endpoint.endswith("/") else endpoint
-        # self.auth = {"Authorization": f"Basic {key}"}
-        # self.client = httpx.AsyncClient(base_url=self.endpoint, headers=self.auth)
+        from lnbits.tasks import catch_everything_and_restart
+        from lnbits.app import settings
+        
+        self.nostr_client = NostrClient()
+        
+        scheduled_tasks: List[Task] = []
+        
+        self.nsec = settings.lnbits_connect_sk
+        self.funding_source_npub = settings.lnbits_connect_pk
+        
+        async def _subscribe_to_nostr_client():
+            # wait for 'nostrclient' extension to initialize
+            await asyncio.sleep(10)
+            await self.nostr_client.run_forever()
+            raise ValueError("Must reconnect to websocket")
 
-    # async def cleanup(self):
-    # try:
-    #     await self.client.aclose()
-    # except RuntimeError as e:
-    #     logger.warning(f"Error closing wallet connection: {e}")
+        async def _wait_for_nostr_events():
+            # wait for this extension to initialize
+            await asyncio.sleep(15)
+            await self.wait_for_nostr_events(self.nostr_client)
+            
+        loop = asyncio.get_event_loop()
+        task1 = loop.create_task(catch_everything_and_restart(_subscribe_to_nostr_client))
+        task2 = loop.create_task(catch_everything_and_restart(_wait_for_nostr_events))
+        scheduled_tasks.extend([task1, task2])
 
     async def status(self) -> StatusResponse:
         logger.warning(
@@ -102,3 +382,64 @@ class LNbitsConnectWallet(Wallet):
                     return message
                 else:
                     print(f"Received another message: {message}")
+                    
+    async def wait_for_nostr_events(self, nostr_client: NostrClient):
+
+        await self.subscribe_to_wallet_service(nostr_client)
+
+        while True:
+            message = await nostr_client.get_event()
+            logger.info(f"Received message: {message}")
+            await self.process_nostr_message(message)
+
+    async def subscribe_to_wallet_service(self, nostr_client: NostrClient):
+        # get env LNBITS_CONNECT_PK value
+        logger.info("Subscribing to wallet service")
+        logger.info(f"Wallet service npub: {settings.lnbits_connect_pk}")
+        public_keys = [settings.lnbits_connect_pk]
+
+        await nostr_client.subscribe_wallet_service(public_keys)
+        
+    async def process_nostr_message(self, msg: str):
+        try:
+            type, *rest = json.loads(msg)
+
+            if type.upper() == "EVENT":
+                _, event = rest
+                event = NostrEvent(**event)
+                if event.kind == 4:
+                    await self._handle_nip04_message(event)
+                return
+
+        except Exception as ex:
+            logger.debug(ex)
+            
+            
+    async def _handle_nip04_message(self, event: NostrEvent):
+        sender_public_key = event.pubkey
+        
+        # TODO: Decrypt the DM and do something depending on the content
+        logger.info(f"Received DM from {sender_public_key}")
+
+        # if not merchant:
+        #     p_tags = event.tag_values("p")
+        #     merchant_public_key = p_tags[0] if len(p_tags) else None
+        #     merchant = (
+        #         await get_merchant_by_pubkey(merchant_public_key)
+        #         if merchant_public_key
+        #         else None
+        #     )
+
+        # assert merchant, f"Merchant not found for public key '{merchant_public_key}'"
+
+        # if event.pubkey == merchant_public_key:
+        #     assert len(event.tag_values("p")) != 0, "Outgong message has no 'p' tag"
+        #     clear_text_msg = merchant.decrypt_message(
+        #         event.content, event.tag_values("p")[0]
+        #     )
+        #     await _handle_outgoing_dms(event, merchant, clear_text_msg)
+        # elif event.has_tag_value("p", merchant_public_key):
+        #     clear_text_msg = merchant.decrypt_message(event.content, event.pubkey)
+        #     await _handle_incoming_dms(event, merchant, clear_text_msg)
+        # else:
+        #     logger.warning(f"Bad NIP04 event: '{event.id}'")
